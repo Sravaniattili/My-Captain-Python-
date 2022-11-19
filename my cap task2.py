0ac2a328cd0ad7eb65f8d408432d65a881e4604a
Sravaniattili <103167465+Sravaniattili@users.noreply.github.com>
2022-11-19 07:46:09 +0530

    Add files via upload

---
+++ my cap task2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+import os
+file_name=input('Input the file name:')
+print(file_name)
+print('The extention of the file is:',file_extension)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ mycap task 1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+r = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(3.14 * r**2))
+
+
+# In[ ]:
+
+
+
+
