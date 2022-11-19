d1d70542812c6223dc37e17ca1cf5e73b30f5870
Sravaniattili <103167465+Sravaniattili@users.noreply.github.com>
2022-11-19 09:10:27 +0530

    Add files via upload

---
+++ my cap task4.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+list1=[12,-7,5,64,-14]
+i=0
+for i in list1:
+    if(i>0):
+        print(i)
+    i+=1
+
+
+# In[5]:
+
+
+list2=[12,14,-95,3]
+i=0
+for i in list2:
+    if(i>0):
+        print(i)
+    i+=1
+
+
+# In[ ]:
+
+
+
+
